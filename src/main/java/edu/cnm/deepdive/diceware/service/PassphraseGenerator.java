ba30163fbc42afb8789549cb65ee8da52872fa85
John B <bohnjailey56@gmail.com>
2019-07-01 13:22:59 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/diceware/service/PassphraseGenerator.java
@@ -0,0 +1,6 @@
+package edu.cnm.deepdive.diceware.service;
+
+public interface PassphraseGenerator {
+  String[] passphrase(int lenght);
+
+}
---
+++ src/main/java/edu/cnm/deepdive/diceware/service/WordProvider.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.diceware.service;
+
+import java.util.Collection;
+
+public interface WordProvider {
+Collection<String> words();
+}
